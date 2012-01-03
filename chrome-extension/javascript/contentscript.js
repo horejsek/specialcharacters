d3e0c410b4d46c25eb6c12747f3489e8e8b4e4f5
Michal Horejsek <horejsekmichal@gmail.com>
2012-01-03 23:38:46 +0100

    support editable div

--- chrome-extension/javascript/contentscript.js
+++ chrome-extension/javascript/contentscript.js
@@ -1,14 +1,15 @@
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    if (request.action == 'appendTextToActiveElement') {
+    if (request.action == 'insertTextToActiveElement') {
         var elm = document.activeElement;
-        elm.value = elm.value + request.text;
 
-    } else if (request.action == 'insertTextToActiveElement') {
-        var elm = document.activeElement,
-            position = elm.selectionStart;
-
-        elm.value = elm.value.substr(0, position) + request.text + elm.value.substr(position, elm.value.length);
-        elm.setSelectionRange(position + 1, position + 1);
+        if (elm.value !== undefined) {
+            var position = elm.selectionStart;
+            elm.value = elm.value.substr(0, position) + request.text + elm.value.substr(position, elm.value.length);
+            elm.setSelectionRange(position + 1, position + 1);
+        } else {
+            // If active element is DIV etc.
+            elm.innerHTML = elm.innerHTML + request.text;
+        }
     }
 });
