e6466181685a393d5f68f012b1941642dc801bad
Michal Horejsek <horejsekmichal@gmail.com>
2012-01-14 11:30:36 +0100

    support for insert characters into active elements in iframe

--- chrome-extension/javascript/contentscript.js
+++ chrome-extension/javascript/contentscript.js
@@ -1,11 +1,56 @@
 
 (function () {
 
-    function insertIntoValueOfElementToPosition(elm, position, value) {
-        elm.value = insertIntoText(elm.value, position, value);
+    function insertText(doc, text) {
+        var elm = doc.activeElement;
+
+        if (elm.tagName == 'IFRAME') {
+            insertText(elm.contentDocument, text);
+        } else if (elm.value !== undefined) {
+            insertTextIntoFormElement(elm, text);
+        } else {
+            insertTextIntoEditableElement(doc, text);
+        }
     }
 
-    function insertIntoText(text, position, subtext) {
+    function insertTextIntoFormElement(elm, text) {
+        var selectionStart = elm.selectionStart,
+            selectionEnd = elm.selectionEnd;
+
+        if (text.length == 2) {
+            selectionEnd++;
+            insertIntoValueOfElementToPosition(elm, selectionStart, text[0]);
+            insertIntoValueOfElementToPosition(elm, selectionEnd, text[1]);
+        } else {
+            insertIntoValueOfElementToPosition(elm, selectionEnd, text);
+        }
+        seekInElemenetToPosistion(elm, selectionEnd + 1);
+    }
+
+    function insertTextIntoEditableElement(win, text) {
+        var selection = win.getSelection(),
+            selectionStartNode = selection.anchorNode,
+            selectionStart = selection.anchorOffset,
+            selectionEndNode = selection.focusNode,
+            selectionEnd = selection.focusOffset;
+
+        if (text.length == 2) {
+            if (selectionStartNode == selectionEndNode) {
+                selectionEnd++;
+            }
+            selectionStartNode.textContent = insertIntoTextToPosition(selectionStartNode.textContent, selectionStart, text[0]);
+            selectionEndNode.textContent = insertIntoTextToPosition(selectionEndNode.textContent, selectionEnd, text[1]);
+        } else {
+            selectionEndNode.textContent = insertIntoTextToPosition(selectionEndNode.textContent, selectionEnd, text);
+        }
+        selection.setPosition(selectionEndNode, selectionEnd + 1);
+    }
+
+    function insertIntoValueOfElementToPosition(elm, position, value) {
+        elm.value = insertIntoTextToPosition(elm.value, position, value);
+    }
+
+    function insertIntoTextToPosition(text, position, subtext) {
         return text.substr(0, position) + subtext + text.substr(position, text.length);
     }
 
@@ -15,38 +60,7 @@
 
     chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         if (request.action == 'insertTextToActiveElement') {
-            var elm = document.activeElement;
-
-            if (elm.value !== undefined) {
-                var selectionStart = elm.selectionStart,
-                    selectionEnd = elm.selectionEnd;
-
-                if (request.text.length == 2) {
-                    insertIntoValueOfElementToPosition(elm, selectionStart, request.text[0]);
-                    // I inserted one character, therefore selectionEnd + 1.
-                    insertIntoValueOfElementToPosition(elm, selectionEnd + 1, request.text[1]);
-                } else {
-                    insertIntoValueOfElementToPosition(elm, selectionEnd, request.text);
-                    // I inserted one character, therefore selectionEnd + 1.
-                    seekInElemenetToPosistion(elm, selectionEnd + 1);
-                }
-
-            } else {
-                // If active element is DIV etc.
-                var selection = window.getSelection(),
-                    selectionStartNode = selection.anchorNode,
-                    selectionStart = selection.anchorOffset,
-                    selectionEndNode = selection.focusNode,
-                    selectionEnd = selection.focusOffset;
-
-                if (request.text.length == 2) {
-                    selectionStartNode.textContent = insertIntoText(selectionStartNode.textContent, selectionStart, request.text[0]);
-                    selectionEndNode.textContent = insertIntoText(selectionEndNode.textContent, selectionEnd, request.text[1]);
-                } else {
-                    selectionEndNode.textContent = insertIntoText(selectionEndNode.textContent, selectionEnd, request.text);
-                }
-                selection.setPosition(selectionEndNode, selectionEnd + 1);
-            }
+            insertText(document, request.text);
         }
     });
 
